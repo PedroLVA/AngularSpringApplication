f2fc163bd1f7bf13d1c13ab4d86e062311c9b6cc
Pedro Athayde <pedro_athayde@outlook.com>
2024-10-01 15:17:31 -0300

    Added form configuration and authService

--- Front/src/app/_Components/login/login.component.ts
+++ Front/src/app/_Components/login/login.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule, RouterOutlet, RouterLinkActive],
+  imports: [RouterModule, RouterOutlet, RouterLinkActive, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  form: FormGroup;
 
+  constructor(){
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    })
+  }
+
+  onSubmit(){
+    if(this.form.valid){
+      console.log(this.form.value);
+    }
+    else{
+      console.log("Errp")
+    }
+  }
+  
 }
